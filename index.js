1343ec8e57adac55358a4e8f01753e6a82e89500
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-03-12 13:20:13 +0300

    simple math

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+// multiplication
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; 
+
+// random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// variables for modulus operation
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; 
+
+// maximum number in a set
+const max = Math.max(5, 10, 15, 20);
+
